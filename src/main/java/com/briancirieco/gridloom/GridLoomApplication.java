2a88fd885ccc1174ce8aeef1ad19bf2671b98244
brian-cirieco <brian.a.cirieco@gmail.com>
2023-12-06 17:48:32 -0800

    Initial commit from Spring Boot Initializr

---
+++ src/main/java/com/briancirieco/gridloom/GridLoomApplication.java
@@ -0,0 +1,13 @@
+package com.briancirieco.gridloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridLoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridLoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briancirieco/gridloom/GridLoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briancirieco.gridloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridLoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
